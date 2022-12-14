36d29564bfde7d638d3507ee0e3bbe8de1c544f7
faith-zuby <zubbyfaith1@gmail.com>
2022-12-14 18:38:18 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * is_lower - checks for lower case character
+ * @c: the character to be checked
+ * Return: 1 for lower case or 0 for anything else
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
